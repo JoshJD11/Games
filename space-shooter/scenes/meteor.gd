eedb9a7a0b9c7da3d318cf584aa1805d9ba0431d
JoshJD11 <josh.temporal11@gmail.com>
2026-01-28 19:23:07 -0600

    my own space shooter progress

---
+++ space-shooter/scenes/meteor.gd
@@ -0,0 +1,35 @@
+extends Area2D
+
+var speed: int
+var rotation_speed: int
+var direction_x: float
+
+func _ready():
+	
+	var rng := RandomNumberGenerator.new() # rng stands for random number generator
+	
+	# texture
+	var random_image_number = rng.randi_range(1, 8)
+	var path: String = "res://graphics/PNG/Meteors/" + str(random_image_number) + ".png"
+	$MeteorImage.texture = load(path)
+	
+	# speed / rotation / direction
+	speed = rng.randi_range(200, 500)
+	rotation_speed = rng.randi_range(40, 100)
+	direction_x = rng.randf_range(-1, 1)
+	
+	#start position
+	var width = get_viewport().get_visible_rect().size[0] # we get the firts value bcs we only care about the width
+	var random_x = rng.randi_range(0, width)
+	var random_y = rng.randi_range(-150, -50)
+	position = Vector2(random_x, random_y)
+
+	
+func _process(delta):
+	position += Vector2(direction_x, 1.0) * speed * delta
+	rotation_degrees += rotation_speed * delta
+	
+
+func _on_body_entered(_body: Node2D) -> void:
+	print("body entered")
+	
